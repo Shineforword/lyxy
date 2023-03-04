c53870833883d24856bc9422be1b11f91bc7fc0e
Qian <dreamingclear@163.com>
2023-03-04 20:41:27 +0800

    [flex - Expanded]

--- lib/explore/constrains/constrains.dart
+++ lib/explore/constrains/constrains.dart
@@ -20,6 +20,7 @@ class Constrains extends StatelessWidget {
           LyxyExploreItem(title: "2.loose松约束", page: Constrain02()),
           LyxyExploreItem(title: "3.tight紧约束", page: Constrain03()),
           LyxyExploreItem(title: "4.unbounded无边界", page: Constrain04()),
+          LyxyExploreItem(title: "5.flex", page: Constrain05()),
         ],
       ),
     );
@@ -175,6 +176,65 @@ class Constrain04 extends StatelessWidget {
                   color: Colors.amber,
                 ),
               )),
+        ));
+  }
+}
+
+class Constrain05 extends StatelessWidget {
+  const Constrain05({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // appBar: LyzyExploreAppBar(navTitle: "子元素充满父类元素"),
+        appBar: AppBar(
+          title: Text("flex"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.code),
+              tooltip: "code",
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => LyxyCodes(
+                        mdpwd:
+                            "lib/assets/markdown/constrains/constrain05.md")));
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              /// 用于 colum row  spacer
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 50,
+                    color: Colors.lightBlue,
+                    child: Center(
+                      child: Text(
+                        "1份",
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                    ),
+                  )),
+              Spacer(
+                flex: 2,
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 50,
+                    color: Colors.lightGreen,
+                    child: Center(
+                      child: Text(
+                        "2份",
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                    ),
+                  )),
+            ],
+          ),
         ));
   }
 }
