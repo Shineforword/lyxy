6f51ebbd714f5966328beaa7f086cc012ee13ab1
Qian <dreamingclear@163.com>
2023-03-16 10:07:52 +0800

    [test - error]

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:lyxy_app/main.dart';
+import 'package:lyxy_app/project/pro_lyxy_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ProLyxyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
